8cdcec2a26f8046fbc44477cc6ff3e2ea8569247
Quentin Rocher <qrocher@weyield.io>
2025-04-04 14:09:43 +0200

    création de la classe Employe avec les attributs donner dans l'uml

---
+++ src/main/java/fr/isep/models/Employe.java
@@ -0,0 +1,18 @@
+package fr.isep.models;
+
+public class Employe extends Personne {
+    private int numeroEmploye;
+    private String dateEmbauche;
+
+    public Employe(int identifiant, String nom, String adresse, String contact, int numeroEmploye, String dateEmbauche) {
+        super(identifiant, nom, adresse, contact);
+        this.numeroEmploye = numeroEmploye;
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public String obtenirRole() {
+        return "Employé";
+    }
+
+    // Getters and Setters
+}
