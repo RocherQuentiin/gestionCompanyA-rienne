5c79cb2b8897d3b059e0ef8b8391d3cd176a8ec0
fathi marie <fathi.marie1@gmail.com>
2025-04-04 16:55:57 +0200

    Methode réservation +tests

--- src/main/java/fr/isep/Main.java
+++ src/main/java/fr/isep/Main.java
@@ -1,5 +1,9 @@
 package fr.isep;
 
+import fr.isep.models.*;
+
+import java.time.LocalDate;
+
 import java.time.LocalDate;
 
 import fr.isep.models.Adresse;
@@ -9,6 +13,28 @@ import fr.isep.models.Pilote;
 
 public class Main {
     public static void main(String[] args) {
+
+        Adresse adresse = new Adresse(10, "Rue de Paris", 75000, "Paris");
+        Passager passager = new Passager(12, "fathi", adresse, "contact", "EU");
+
+        Aeroport origine = new Aeroport("Charles de Gaulle", "Paris", "Aéroport international");
+        Aeroport destination = new Aeroport("JFK", "New York", "Aéroport américain");
+
+
+        Avion avion1 = new Avion("Airbus A350", "280", 25);
+
+        Employe e1 = new Employe(1, "Alice Dupont", adresse, "alice@aeroport.fr", 1001, LocalDate.of(2020, 5, 10));
+        Employe e2 = new Employe(2, "Bob Martin", adresse, "bob@aeroport.fr", 1002, LocalDate.of(2021, 3, 15));
+
+        Vol vol = new Vol(787,origine,destination, "2024-05-05", "2025-04-04", "dispo" );
+        Reservation res = new Reservation(123,  LocalDate.parse("2025-04-04"), "dispo", vol, passager);
+        Reservation res2 = new Reservation(123,  LocalDate.parse("2025-04-04"), "confirmé", vol, passager);
+
+        res.confirmerReservation();
+        res2.annulerReservation();
+        System.out.println(passager.getReservations());
+
+
         Adresse adresse = new Adresse(10, "Rue de Paris", 75000, "Paris");
 
         // Création d'un pilote
