72206d22b91785c62dca6838478efc51875b5ba3
Quentin Rocher <qrocher@weyield.io>
2025-04-04 14:08:56 +0200

    créationd ela classe Aeorport avec les attributs donner dans l'uml

---
+++ src/main/java/fr/isep/models/Aeroport.java
@@ -0,0 +1,19 @@
+package fr.isep.models;
+
+public class Aeroport {
+    private String nom;
+    private String ville;
+    private String description;
+
+    public Aeroport(String nom, String ville, String description) {
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+    }
+
+    public void affecterVol() {
+        System.out.println("Vol affecté à l'aéroport.");
+    }
+
+    // Getters and Setters
+}
